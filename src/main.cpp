2993f72b202bf305a9c02ab6b61c976bd4a2f2fc
Emilie Schott <emilie.schott@gmail.com>
2025-05-23 16:55:22 +0200

    1 - Forces, Interpolation, Easing : Forces

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,22 @@
 #include "utils.hpp"
 
 struct Particle {
-    glm::vec2 position = glm::vec2(utils::rand(-gl::window_aspect_ratio(), +gl::window_aspect_ratio()), utils::rand(-1, 1));
-    // int x = utils::rand(-gl::window_aspect_ratio(), +gl::window_aspect_ratio());
-    // int y = utils::rand(-1, 1);
+    glm::vec2 position = glm::vec2(utils::rand(-gl::window_aspect_ratio(), +gl::window_aspect_ratio()), utils::rand(-1.f, 1.f));
     float radius = .01f; 
     glm::vec4 color = glm::vec4(.2f, .2f, 1.f, 1.f);
+    glm::vec2 speedVector = determineVectorFromPolarCoordinates();
+
+    glm::vec2 determineVectorFromPolarCoordinates() {
+        float angle = utils::rand(0.f, 360.f);
+        float vectorLength = utils::rand(0.01f, 0.05f);
+
+        return glm::vec2(glm::cos(angle)*vectorLength, glm::sin(angle)*vectorLength);
+    };
+
+    void moveParticle(float deltaTime) {
+        position.x += speedVector.x * deltaTime;
+        position.y += speedVector.y * deltaTime;
+    };
 };
 
 int main()
@@ -28,11 +39,11 @@ int main()
         glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        for(Particle particle : particleArray){
-            utils::draw_disk(particle.position, particle.radius, particle.color);
-        }
-
         // TODO update particles
         // TODO render particles
+        for(Particle& particle : particleArray){
+            particle.moveParticle(gl::delta_time_in_seconds());
+            utils::draw_disk(particle.position, particle.radius, particle.color);
+        }
     }
 }
