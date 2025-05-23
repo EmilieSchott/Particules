4955aef0e4f220dcb8316f8f13278b70e69998a9
Emilie Schott <emilie.schott@gmail.com>
2025-05-23 15:56:14 +0200

    1 - Forces, Interpolation, Easing : Setup initial

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include "opengl-framework/opengl-framework.hpp"
 #include "utils.hpp"
 
+struct Particle {
+    glm::vec2 position = glm::vec2(utils::rand(-gl::window_aspect_ratio(), +gl::window_aspect_ratio()), utils::rand(-1, 1));
+    // int x = utils::rand(-gl::window_aspect_ratio(), +gl::window_aspect_ratio());
+    // int y = utils::rand(-1, 1);
+    float radius = .01f; 
+    glm::vec4 color = glm::vec4(.2f, .2f, 1.f, 1.f);
+};
+
 int main()
 {
     gl::init("Particules!");
@@ -9,11 +17,20 @@ int main()
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
 
     // TODO: create an array of particles
+    std::vector<Particle> particleArray;
+    for(int i = 0; i < 100; ++i){
+        Particle particle;
+        particleArray.push_back(particle);
+    }
 
     while (gl::window_is_open())
     {
         glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        for(Particle particle : particleArray){
+            utils::draw_disk(particle.position, particle.radius, particle.color);
+        }
 
         // TODO update particles
         // TODO render particles
